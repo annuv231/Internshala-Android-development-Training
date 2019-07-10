a1d1600bddc021747cf7b1c585c0e065c7170252
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2019-07-10 06:16:55 +0530

    Add files via upload

---
+++ MyProfile Module 1/app/src/main/java/com/example/myprofile/education.kt
@@ -0,0 +1,12 @@
+package com.example.myprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class education : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_education)
+    }
+}
