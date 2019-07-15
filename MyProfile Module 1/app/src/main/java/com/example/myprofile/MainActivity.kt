fd825f316ea5c83aa2a499aade329a35218b337d
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2019-07-15 19:18:26 +0530

    Added click Listener

--- MyProfile Module 1/app/src/main/java/com/example/myprofile/MainActivity.kt
+++ MyProfile Module 1/app/src/main/java/com/example/myprofile/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.myprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-
+          var photo: ImageView?=null
+          var edubutton: Button?=null
+           var workButton: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.homescreen)
+        photo=findViewById(R.id.profileText)
+        photo?.setOnClickListener({
+            var clickIntent=Intent(this@MainActivity,profilePhoto::class.java)
+            startActivity(clickIntent)
+        })
+        edubutton=findViewById((R.id.eductationId))
+        edubutton?.setOnClickListener({
+            var clickIntent=Intent(this@MainActivity,education::class.java)
+            startActivity((clickIntent))
+        })
+        workButton=findViewById(R.id.workexpId)
+        workButton?.setOnClickListener({
+            var clickIntent=Intent(this@MainActivity,workexperience::class.java)
+            startActivity(clickIntent)
+        })
     }
 }
